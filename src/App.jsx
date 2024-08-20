0d6056488e85f15b8185f022d805afa8d83baab3
Tyler Olsen <tylerkolsen@gmail.com>
2024-08-20 14:02:09 -0600

    Finished through step 5

--- src/App.jsx
+++ src/App.jsx
@@ -1,3 +1,44 @@
+import { useState, useEffect } from "react"
+import axios from "axios"
+
 export default function App() {
-  return <div>Hello</div>;
+  const [friends, setFriends] = useState([])
+  const [picture, setPicture] = useState('')
+  const [name, setName] = useState('')
+
+  const addFriend = (e) => {
+    e.preventDefault()
+    setFriends(
+      [...friends, {
+        picture: picture,
+        name: name
+      }]
+    )
+    setPicture('')
+    setName('')
+  }
+
+  const friendInfo = friends.map((friendsObj) => {
+   return (
+    <div key={friendsObj.name}>
+      <img src={friendsObj.picture} alt={`Picture of ${friendsObj.name}`} width={200} height={200}/>
+      <span>{friendsObj.name}</span>
+    </div>
+    )
+  })
+
+  return (
+  <>
+    <div>
+        <label htmlFor="picture">Picture: </label>
+        <input type="text" id="picture" value={picture} onChange={(e) => setPicture(e.target.value)}/>
+
+        <label htmlFor="name">Name: </label>
+        <input type="text" id="name" value={name} onChange={(e) => setName(e.target.value)}/>
+
+        <button type="submit" onClick={addFriend}>Add Friend</button>
+    </div>
+    {friendInfo}
+  </>
+  )
 }
